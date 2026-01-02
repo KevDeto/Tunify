780ab1f86a3beea7d25d71e8860f1b6496e81795
kevin <contactohuayta@gmail.com>
2026-01-02 11:00:09 -0300

    error de ruta solucionado

--- Tunify/src/App.jsx
+++ Tunify/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react"
-import Header from "./components/Layout/Header/Header"
-import Resizable from "@/components/ui/Resizable/Resizable"
-import PlayerBar from "./components/Layout/PlayerBar/PlayerBar"
+import Header from "@/components/Layout/Header/Header.jsx"
+import Resizable from "@/components/ui/Resizable/Resizable.jsx"
+import PlayerBar from "@/components/Layout/PlayerBar/PlayerBar.jsx"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
